92986729c6cd7cb0a7eda4394bbf9bca8634716e
Krasnoperov Konstantin <krasnoperovkonstantin@gmail.com>
2021-01-05 15:20:18 +0400

    создана страница и структура

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE HTML>
+<html>
+ <head>
+  <meta charset="utf-8">
+  <title>Анкета</title>
+  <link rel="stylesheet" href="/css/main.css">
+  <script src="/js/jquery-3.5.1.slim.min.js"></script>
+  <script src="/js/main.js"></script>
+ </head>
+ <body>
+  <div class="wrap">
+   <div class="content">
+     <form action="insertdata.php">
+       <p>Введите данные о человеке:</p>
+       <label for="firstname" class="label">Имя</label>
+       <input type="text" name="firstname" id="firstname" value="" placeholder="Имя" class="input"><br>
+       <label for="firstname" class="label">Фамилия</label>
+       <input type="text" name="lastname" id="lastname" value="" placeholder="Фамилия" class="input"><br>
+       <label for="firstname" class="label">Возраст</label>
+       <input type="text" name="age" id="age" value="" placeholder="Возраст" class="input"><br>
+       <button type="submit" name="save" id="save" class="button">сохранить</button>
+       <button type="submit" name="upload" id="upload" class="button">выгрузить</button>
+     </form>
+   </div>
+  </div>
+ </body>
+</html>
